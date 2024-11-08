7bc9a6f25336223f3eea1779a76740c9c92a5480
Siim Puidet <siim.puidet@tktk.ee>
2024-11-08 10:17:35 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,22 @@
+﻿// Rakendus küsib kasutaja vanust
+Console.WriteLine("Enter your age");
+string userAge = Console.ReadLine();
+int userAgeNum = 0;
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum); // boolean - true/false
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+//int userAge = Int32.Parse(Console.ReadLine()); //"Int" sisestab vahemällu numbri, mis laseb järgmistes sammudes kasutada matemaatilisi lahendeid. "String" sisestaks sõna ja suurem/väiksem tehted ei toimiks.
+
+// Kui vanus on sisestatud numbriliselt, antakse üks kahest väljundist:
+if (isAgeNumber == true)
+{
+    // Kui kasutaja vanus on väehmalt 13, siis konsoolis kuvatakse: "Welcome to Instagram!".
+    if (userAgeNum >= 13) { Console.WriteLine("Welcome to Instagram!"); }
+    //if(userAge >= 13) {Console.WriteLine("Welcome to Instagram!");}
+   
+    // Muul juhul kuvatakse: "You are too young to use Instagram.".
+    else { Console.WriteLine("You are too young to use Instagram."); }
+    //else {Console.WriteLine("You are too young to use Instagram.");}
+}
+//Kui vanus on sisestatud muul kujul kui ainult numbrid, antakse kasutajale teada, et vanust ei õnnestunud tuvastada.
+else { Console.WriteLine("Could not read your age."); }
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,17 @@
+﻿// Rakendus küsib kasutaja sugu (m/f)
+using System.Data;
+
+Console.WriteLine("Please, select your gender (m/f):");
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolist andmeid string formaadis
+
+// Rakendus küsib kasutaja perekonnanime
+Console.WriteLine("Please enter your last name");
+string userLastName = Console.ReadLine();
+
+// Rakendus tervitab kasutajat, lähtuvalt kasutaja valikutest
+if(userGender == 'm')
+{Console.WriteLine($"Welcome, Mr. {userLastName}!");} // Kui on sisestatud "m"
+else if(userGender == 'f')
+{Console.WriteLine($"Welcome, Ms. {userLastName}!");} // Kui on sisestatud "f"
+else
+{Console.WriteLine($"Welcome, {userLastName}!");} // Kui on sisestatud mingi muu täht
